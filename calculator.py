d53ca2f07f47ee2536cb216791673864749a6c1a
Michael Newbould <michael_newbould@hotmail.com>
2019-10-10 22:03:07 +0200

    Display latest transaction

--- calculator.py
+++ calculator.py
@@ -9,12 +9,16 @@ amount = 0
 with open(file) as csvread:
 	read = csv.reader(csvread, delimiter=';')
 	next(read)
+	hasRead = False
 	for row in read:
 		description = row[1]
 		if "Transactie" in description:
 			identifier = description.split("(Transactie ")[1][:-1]
+			if hasRead == False:
+				print("Last transaction is now: " + identifier + ", Remember this!")
 			if id == identifier:
 				break
 			amt = row[2][2:-4]
 			amount += float(amt)
+		hasRead = True
 print("Total amount: " + str(amount))  
