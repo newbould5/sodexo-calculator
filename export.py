8055b937a3e732afbf80b8dd8974ea1846e62805
Michael Newbould <michael_newbould@hotmail.com>
2020-01-27 22:41:33 +0100

    they stop robots now

--- export.py
+++ export.py
@@ -3,6 +3,8 @@ from mechanize import Browser
 import config
 
 browser = Browser()
+browser.set_handle_robots(False)
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 browser.open("https://sodexo4you.be/nl")
 
 browser.select_form(id="account-login")
